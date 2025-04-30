c23d897c51182df66e3ecd7e5359424bc7fbff0d
Israel Nunes <israelnetonunes@gmail.com>
2025-04-30 09:40:27 -0300

    feat: install and implement cors policy to avoid conflict with frontend

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/routes.js';
 // import { connectDatabase } from '../database/config/dbConnect.js';
 
@@ -14,6 +15,7 @@ import router from './routes/routes.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
--- api/controllers/countryStateApi.js
+++ api/controllers/countryStateApi.js
@@ -4,7 +4,7 @@ dotenv.config();
 export async function getApi (req, res) {
     try {
         const apiKey = process.env.COUNTRY_STATE_KEY;
-        res.status(200).send(apiKey);
+        res.status(200).json({ apiKey });
     } catch (error) {
         console.error('Error retrieving API Key:', error);
         res.status(500).send('Error retrieving API Key')
